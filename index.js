02171bb592bd334152a09b35cc70a39e9cd62cc2
Tiago Quelhas <tiagoq@gmail.com>
2015-05-17 16:02:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function EventEmitter() {}
+
+EventEmitter.prototype.addEventListener = function(event, fn) {
+  var eventMap = this.__events = this.__events || {};
+  var handlerList = eventMap[event] = eventMap[event] || [];
+  handlerList.push(fn);
+};
+
+EventEmitter.prototype.removeEventListener = function(event, fn) {
+  var eventMap = this.__events = this.__events || {};
+  var handlerList = eventMap[event];
+  if (handlerList) {
+    var index = handlerList.indexOf(fn);
+    if (index >= 0) {
+      handlerList.splice(index, 1);
+    }
+  }
+};
+
+EventEmitter.prototype.emit = function() {
+  var eventMap = this.__events = this.__events || {};
+  var event = arguments[0];
+  var handlerList = eventMap[event];
+  if (handlerList) {
+    for (var i = 0; i < handlerList.length; i++) {
+      var fn = handlerList[i];
+      fn.apply(this, arguments);
+    }
+  }
+};
+
+EventEmitter.mixin = function(ctor) {
+  for (var prop in EventEmitter.prototype) {
+    if (EventEmitter.prototype.hasOwnProperty(prop)) {
+      ctor.prototype[prop] = EventEmitter.prototype[prop];
+    }
+  }
+};
+
+module.exports = EventEmitter;
